30a9c29c27c11c989f8b54b63a85e910b52fb0ef
dongxingtao <dxt@slofzx.com>
2023-10-24 10:20:31 +0800

    yes

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-'''
-建立streamlit和本地vscode的自动连接
 
-20231024测试success
-'''
-chart_data = pd.DataFrame(
-   {
-       "col1": np.random.randn(20),
-       "col2": np.random.randn(20),
-       "col3": np.random.choice(["A", "B", "C"], 20),
-   }
-)
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
-st.area_chart(chart_data, x="col1", y="col2")
+st.bar_chart(chart_data)
