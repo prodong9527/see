1c391eb601c14b5a2f7ec1f9d83f4403075f4397
dongxingtao <dxt@slofzx.com>
2023-10-24 17:38:24 +0800

    c

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from sklearn import datasets
 
+iris = datasets.load_iris()
 st.title('This is a title')
 st.title('_Streamlit_ is :blue[cool] :sunglasses:')
+st.markdown('# This is markdown')
