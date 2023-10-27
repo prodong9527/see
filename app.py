a0457014a405ac4cbb3f70ee8ef3568678c2f18e
dongxingtao <dxt@slofzx.com>
2023-10-27 16:57:39 +0800

    a

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+'''
+## 程程，这是专门给你发布的应用
+## 老爸准备有空就尝试下streamlit的数据可视化功能，效果随时发在这里。
+'''
+
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 st.scatter_chart(chart_data)
