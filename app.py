e92c53b22f3d1b19c91370c928d6908ee1eaabfc
dongxingtao <dxt@slofzx.com>
2023-10-30 11:01:57 +0800

    a

--- app.py
+++ app.py
@@ -51,17 +51,4 @@ if chat:
 else:
     with st.container():
         st.warning("Please set your OpenAI API key in the settings page.")
-=======
-muti='''
-今天是10.28，今明两天我将尝试完成这个应用的版面结构设计，开始生成和展示数据
-'''
-st.markdown(muti)
 
-add_radio=st.sidebar.radio(
-    "select",("one","two","three")
-)
-
-# df=pd.DataFrame(np.random.randn(50,20),columns=("第%d列" % i for i in range(20)))
-df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
-st.dataframe(df.style.highlight_max(axis=0))
->>>>>>> 95ffa71852e7f57b72485714112c438396ab6237
