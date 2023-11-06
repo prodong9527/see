db737751dee24e8b5fa9f636aa22d88814233b0d
dongxingtao <dxt@slofzx.com>
2023-11-06 16:00:17 +0800

    a

--- pages/1_👨‍👩‍👧_HappyBirthday.py
+++ pages/1_👨‍👩‍👧_HappyBirthday.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image
 
-image=Image.open('images/IMG_20170513_082907.jpg')
+
+image=Image.open('/images/IMG_20170513_082907.jpg')
 st.image(image,caption='wife')
