804417953e732d93e7b81da1ec64ad92aee2ff55
dongxingtao <dxt@slofzx.com>
2023-11-06 17:26:37 +0800

    a

--- pages/1_👨‍👩‍👧_HappyBirthday.py
+++ pages/1_👨‍👩‍👧_HappyBirthday.py
@@ -5,9 +5,9 @@ from PIL import Image
 st.set_page_config(page_title="幸福记忆", layout="wide")
 
 st.markdown("## 这里有永远留在我们记忆里的幸福 &mdash;\
-            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom: \
-            --- " 
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:  " 
             )
+st.markdown("----------")
 image_folder='images'
 # 获取文件夹中的所有图片文件
 image_files = [f for f in os.listdir(image_folder) if f.endswith(('.jpg', '.jpeg', '.png'))]
