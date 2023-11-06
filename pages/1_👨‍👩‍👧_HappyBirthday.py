5241cb64e542df81a48a375224070b23f5770681
dongxingtao <dxt@slofzx.com>
2023-11-06 17:14:19 +0800

    a

--- pages/1_👨‍👩‍👧_HappyBirthday.py
+++ pages/1_👨‍👩‍👧_HappyBirthday.py
@@ -2,8 +2,9 @@ import streamlit as st
 import os
 from PIL import Image
 
-st.set_page_config(page_title="天鱼精选", layout="wide")
+st.set_page_config(page_title="幸福记忆", layout="wide")
 
+st.header('这里有永远留在我们记忆中的:blue[幸福]:sunglasses:', divider='rainbow')
 image_folder='images'
 # 获取文件夹中的所有图片文件
 image_files = [f for f in os.listdir(image_folder) if f.endswith(('.jpg', '.jpeg', '.png'))]
