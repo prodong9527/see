2532c1ff84cd52e89bd9c077eec510a2c454d77e
dongxingtao <dxt@slofzx.com>
2023-11-06 11:23:10 +0800

    a

--- pages/2_API_key.py
+++ pages/2_API_key.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 if "OPENAI_API_KEY" not in st.session_state:
-    st.session_state["OPENAI_API_KEY"] = "c"
+    st.session_state["OPENAI_API_KEY"] = "ccc"
 
 st.set_page_config(page_title="OpenAI Settings", layout="wide")
 
