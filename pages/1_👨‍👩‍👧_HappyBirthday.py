06f6e2a0293405743b7b5c373195c6db1f70275e
dongxingtao <dxt@slofzx.com>
2023-11-06 16:45:12 +0800

    a

--- pages/1_👨‍👩‍👧_HappyBirthday.py
+++ pages/1_👨‍👩‍👧_HappyBirthday.py
@@ -8,19 +8,17 @@ image_folder='images'
 # 获取文件夹中的所有图片文件
 image_files = [f for f in os.listdir(image_folder) if f.endswith(('.jpg', '.jpeg', '.png'))]
 # 设置图片尺寸
-# image_size = (300, 300)
+image_size = (300, 300)
 
 # 初始化图片索引
 image_index = 0
 
 # 循环播放图片
-while True:
+for image_index in image_files:
     # 读取并调整图片尺寸
-    image = Image.open(os.path.join(image_folder, image_files[image_index]))
-    # image = image.resize(image_size)
+    image = Image.open(os.path.join(image_folder, image_index))
+    image = image.resize(image_size)
     
     # 显示图片
     st.image(image)
-    
-    # 更新图片索引
-    image_index = (image_index + 1) % len(image_files)
+
