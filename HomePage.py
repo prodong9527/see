8cd55dadfb3960ab25581be0d70918ac3c4b30b6
dongxingtao <dxt@slofzx.com>
2023-11-07 13:51:14 +0800

    a

--- HomePage.py
+++ HomePage.py
@@ -31,7 +31,8 @@ if chat:
             elif isinstance(message, AIMessage):
                 with st.chat_message("assistant"):
                     st.markdown(message.content)
-        prompt = st.chat_input("Type something...")+prompt_l
+        prompt = st.chat_input("Type something...")
+        prompt
         if prompt:
             st.session_state["messages"].append(HumanMessage(content=prompt))
             with st.chat_message("user"):
